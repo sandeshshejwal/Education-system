2ff4d911abdc701948f4bca473dbbdc6bf960039
Sandesh Shejwal <sandeshshejwal659@gmail.com>
2024-08-22 12:56:14 +0530

    login.js

--- login.js
+++ login.js
@@ -5,7 +5,9 @@ document.getElementById('loginForm').onsubmit = function(event) {
     const password = document.getElementById('password').value;
 
     if (username && password) {
-        window.location.href = '/Users/sandeshshejwal/Desktop/Deloitte/DAY4/ed/index.html';
+        document.getElementById('loginMessage').textContent = '';
+
+        window.location.href = 'index.html'; 
     } else {
         document.getElementById('loginMessage').textContent = 'Please enter both username and password.';
     }
